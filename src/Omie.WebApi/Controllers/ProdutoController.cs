0fb575a5ed1edd01db34f75159d2c1f249494a8f
gmottajr <jrmottagerson@gmail.com>
2025-02-14 02:15:09 -0300

    Initial: Adding presentation layer and solution file

---
+++ src/Omie.WebApi/Controllers/ProdutoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Omie.Application;
+using Omie.Application.Models;
+
+namespace Omie.WebApi;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProdutoController : OmieVendasBaseController<ProdutoDto, long>
+{
+    public ProdutoController(IProdutoAppService applicationService) : base(applicationService)
+    {
+    }
+}
---
+++ src/Omie.WebApi/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.OpenApi.Models;
+using Omie.IoC;
+namespace Omie.WebApi;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.DbContextDiRegistration(builder.Configuration);
+        builder.Services.AddApplicationServices();
+        builder.Services.DataRepositoriesDiRegistration();
+        builder.Services.AddConfigurationSettings(builder.Configuration);
+
+        // Add services to the container
+        builder.Services.AddControllers();
+
+        builder.Services.AddSwaggerGen(c => {
+            c.SwaggerDoc("v1", new OpenApiInfo { 
+                Title = "Omie Vendas - WebApi", 
+                Version = "v1",
+                Description = "Omie Vendas - WebApi",
+                Contact = new OpenApiContact {
+                    Name = "Gerson Jr",
+                    Email = "contact@Omie.com",
+                    Url = new Uri("https://www.Omie.com")
+                }
+                });
+        });
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Omie Vendas - WebApi");
+                c.RoutePrefix = string.Empty;
+            });
+        }
+
+        // Configure the HTTP request pipeline
+        app.UseRouting();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        app.Run();
+    }
+}
